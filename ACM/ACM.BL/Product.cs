ab21611df141940aa3eb96a7fd58fc55f2c9b1ef
Rehan Memon <rehan4747@gmail.com>
2021-02-08 12:33:23 +0530

    Completed Course

--- ACM/ACM.BL/Product.cs
+++ ACM/ACM.BL/Product.cs
@@ -2,7 +2,7 @@
 
 namespace ACM.BL
 {
-  public class Product : EntityBase
+  public class Product : EntityBase, ILoggable
   {
     public Product()
     {
@@ -29,6 +29,9 @@ namespace ACM.BL
         _productName = value;
       }
     }
+
+    public string Log() =>
+    $"{ProductId}: {ProductName} Detail: {ProductDescription} Status: {EntityState.ToString()}";
 
     public override string ToString() => ProductName;
 
